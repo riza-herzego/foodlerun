3abc3f9566dbf8777e9f7b19e225e9ac53873d8d
suci23 <sucifadhilah23@gmail.com>
2012-07-29 14:01:50 +0700

    Update pCanvas.java

--- pCanvas.java
+++ pCanvas.java
@@ -5,44 +5,67 @@ import javax.microedition.lcdui.Graphics;
 import javax.microedition.lcdui.Image;
 import javax.microedition.lcdui.game.GameCanvas;
 import javax.microedition.lcdui.game.Sprite;
+import javax.microedition.lcdui.game.TiledLayer;
 
 public class pCanvas extends GameCanvas implements Runnable {
 	Thread t;
 	Graphics g;
-	
-	int height = Math.max(super.getWidth(), super.getHeight());
-    int width = Math.min(super.getWidth(), super.getHeight());
-    
+
+	int width = 240;
+    int height = 320;
+
     int skor = 0;
-	
-	int posXimg1 = 0, posXimg2 = height, posXimg3 = height*2;
-	int posXAwan = 0, posXAwan2 = height, posXAwan3 = height*2;
-	
+
+	int posYimg1 = 0, posYimg2 = height, posYimg3 = height*2;
+	int posYAwan1 = 0, posYAwan2 = height, posYAwan3 = height*2;
+	int posYRoad1 = 0, posYRoad2 = 80, posYRoad3 = width+50;
+
 	Image bg;
 	Image awan;
 	//sprite
 	Image runImg;
-	Sprite spRun;	
-    
+	Sprite spRun;
+
 	int[] seqRun = new int[] {0,0,1,1,2,2};
 	int cX, cY;
 
 	//koin
 	Image koin;
-	Sprite koins;
-	boolean tampak = true;	
+	TiledLayer koins;
+	
+	private static int[][] arrKoin = {
+		{1,1,1,1,1,1,1,1,1,1,1,1},
+		{1,1,1,1,1,1,1,1,1,1,1,1},
+		{1,1,1,1,2,1,1,1,1,1,1,1},
+		{1,1,1,1,2,1,1,1,1,1,1,1},
+		{1,1,1,1,2,1,1,1,1,1,1,1},
+		{1,1,1,1,1,1,1,1,1,1,1,1},
+		{1,1,1,1,1,1,1,1,1,1,1,1},
+		{1,1,1,1,1,1,1,1,1,1,1,1},
+		{1,1,1,1,1,1,1,1,1,1,1,1},
+		{1,1,1,1,1,1,1,1,1,1,1,1},
+		{1,1,1,1,1,1,1,1,1,1,1,1},
+		{1,1,1,1,1,1,1,1,1,1,1,1},
+		{1,1,1,1,1,1,1,1,1,1,1,1},
+		{1,1,1,1,1,1,1,1,1,1,1,1},
+		{1,1,1,1,1,1,1,1,1,1,1,1},
+		{1,1,1,1,1,1,1,1,1,1,1,1}
+	};
 	
 	//logic of jump
 	boolean jumping = false;
 	boolean falling = false;
 	int tinggi = 12;
-		
+
 	//road
 	Image road;
-	
+	Image road2;
+	Image road3;
+	Sprite roads, roads2, roads3;
+
 	//boolean untuk memeriksa apakah char sedang di atas road atau tidak
     boolean onRoad = false;
-	
+
 	protected pCanvas() {
 		super(true);
 		g = getGraphics();
@@ -53,14 +76,15 @@ public class pCanvas extends GameCanvas implements Runnable {
 	public void run() {
 		initImage();
 		initChar();
-		while (true) {
+		initKoin();
+		while (true) {			
 			drawMatahari();
 			drawAwan();
+			drawRoad();
 			drawChar();
-			drawKoin();
 			g.setColor(0, 0, 0);
 			g.setFont(Font.getFont(Font.FACE_PROPORTIONAL, Font.STYLE_BOLD, Font.SIZE_MEDIUM));
-			g.drawString("Skor: "+skor, width, 0, Graphics.TOP | Graphics.RIGHT );
+			g.drawString("Skor: "+skor, width, 0, Graphics.TOP | Graphics.RIGHT);
 			jump();
 			gameInput();
 			flushGraphics();
@@ -69,65 +93,166 @@ public class pCanvas extends GameCanvas implements Runnable {
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
-			
+
 		}
 	}
-	
+
 	void startGame(){
 		t.start();
 	}
-	
+
 	void initImage(){
 		try {
 			bg = Image.createImage("/sky-tile.png");
 			awan = Image.createImage("/matahari.png");
-			runImg = Image.createImage("/bakso.png");	
-			koin = Image.createImage("/koinsp.png");
-			road = Image.createImage("/road1-tile.png");
+			runImg = Image.createImage("/bakso.png");
+			road = Image.createImage("/1nyah.png");				
+			road2 = Image.createImage("/road1-tile.png");
+			road3 = Image.createImage("/road3-tile.png");
+			koin = Image.createImage("/koinn.png");
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
-	}	
-	
+
+	}
+
 	void initChar(){
 		spRun = new Sprite(runImg, 44, 44);
-		koins = new Sprite(koin,20,20);
+		roads = new Sprite (road, 108, 60);
+		roads2 = new Sprite (road2, 132, 80);
+		roads3 = new Sprite (road3, 124, 80);
 		spRun.setFrameSequence(seqRun);
-		koins.setFrameSequence(seqRun);
-		cX = 10;
-		cY = height-runImg.getHeight();
+		cX = 30;
+		cY = 10;
 	}
 	
+	void initKoin(){
+		koins = new TiledLayer(12, 16, koin, 20, 20);
+		for(int row = 0; row < 16; ++row) {
+			for(int column = 0; column < 12; ++column) {
+				koins.setCell(column, row, arrKoin[row][column]);
+			}
+		}
+		
+	}
+
 	void drawChar(){
+		spRun.setTransform(5);
 		spRun.setPosition(cX, cY);
-		if (!jumping && !falling) {
-			spRun.nextFrame();
-		}else
-			spRun.setFrame(0);	
-		spRun.paint(g);			
-	}
-	
-	void drawKoin(){
-		koins.setPosition(posXimg2, height-100);
-		koins.nextFrame();
-		koins.paint(g);
-		if(spRun.collidesWith(koins, true)){
+		spRun.paint(g);
+		if(spRun.collidesWith(koin, 0, 0, true)){
 			skor+=100;
-			koins.setVisible(!tampak);			
-		} 
-	}	
-	
+		}
+	}
+
 	void gameInput(){
 		int keystate = getKeyStates();
-		
-		if (((keystate & LEFT_PRESSED) != 0)) {
-				spRun.setTransform(2);
-				cX -= 3;
+
+		if (((keystate & UP_PRESSED) != 0)) {
+			spRun.setTransform(2);
+			if (!jumping && !falling) {
+				spRun.nextFrame();
+			}else
+				spRun.setFrame(0);
+			
+			posYimg1+=3;
+			posYimg2+=3;
+			posYimg3+=3;
+
+			if (posYimg1 < -height) {
+				posYimg1 = height;
+			}
+			if (posYimg2 < -height) {
+				posYimg2 = height;
+			}
+			if (posYimg3 < -height) {
+				posYimg3 = height;
+			}
+
+			posYAwan1+=1;
+			posYAwan2+=1;
+			posYAwan3+=1;
+
+			if (posYAwan1 < -height) {
+				posYAwan1 = height;
+			}
+			if (posYAwan2 < -height) {
+				posYAwan2 = height;
+			}
+			if (posYAwan3 < -height) {
+				posYAwan3 = height;
+			}
+			
+			posYRoad1+=3;
+			posYRoad2+=3;
+			posYRoad3+=3;
+
+			if (posYRoad1 < -width) {
+				posYRoad1 = width;
+			}
+			if (posYRoad2 < -width) {
+				posYRoad2 = width;
+			}
+			if (posYRoad3 < -width) {
+				posYRoad3 = width;
+			}		
+
+			koins.move(0, 3);
+
 		}
-		if (((keystate & RIGHT_PRESSED) != 0)) {
-				spRun.setTransform(0);
-				cX += 3;
+		
+		if (((keystate & DOWN_PRESSED) != 0)) {
+			
+			spRun.setTransform(0);
+			if (!jumping && !falling) {
+				spRun.nextFrame();
+			}else
+				spRun.setFrame(0);
+			
+			posYimg1-=3;
+			posYimg2-=3;
+			posYimg3-=3;
+
+			if (posYimg1 < -height) {
+				posYimg1 = height;
+			}
+			if (posYimg2 < -height) {
+				posYimg2 = height;
+			}
+			if (posYimg3 < -height) {
+				posYimg3 = height;
+			}
+
+			posYAwan1-=1;
+			posYAwan2-=1;
+			posYAwan3-=1;
+
+			if (posYAwan1 < -height) {
+				posYAwan1 = height;
+			}
+			if (posYAwan2 < -height) {
+				posYAwan2 = height;
+			}
+			if (posYAwan3 < -height) {
+				posYAwan3 = height;
+			}
+			
+			posYRoad1-=3;
+			posYRoad2-=3;
+			posYRoad3-=3;
+
+			if (posYRoad1 < -width) {
+				posYRoad1 = width;
+			}
+			if (posYRoad2 < -width) {
+				posYRoad2 = width;
+			}
+			if (posYRoad3 < -width) {
+				posYRoad3 = width;
+			}		
+
+			koins.move(0, -3);
+			
 		}
 		if ((keystate & FIRE_PRESSED) != 0) {
 			if (!jumping && !falling) {
@@ -135,74 +260,75 @@ public class pCanvas extends GameCanvas implements Runnable {
 			}
 		}
 	}
-	
+
 	void drawAwan(){
+			g.drawRegion(bg, 0, 0, bg.getWidth(), bg.getHeight(), 5, 0, posYimg1, Graphics.LEFT | Graphics.BOTTOM);
+			g.drawRegion(bg, 0, 0, bg.getWidth(), bg.getHeight(), 5, 0, posYimg2, Graphics.LEFT | Graphics.BOTTOM);
+			g.drawRegion(bg, 0, 0, bg.getWidth(), bg.getHeight(), 5, 0, posYimg3, Graphics.LEFT | Graphics.BOTTOM);			
 			
-			g.drawImage(bg, posXimg1, height, Graphics.LEFT | Graphics.BOTTOM);
-			g.drawImage(bg, posXimg2, height, Graphics.LEFT | Graphics.BOTTOM);
-			g.drawImage(bg, posXimg3, height, Graphics.LEFT | Graphics.BOTTOM);			
-			
-			posXimg1-=3;
-			posXimg2-=3;
-			posXimg3-=3;
-			
-			if (posXimg1 < -height) {
-				posXimg1 = height;
-			}
-			if (posXimg2 < -height) {
-				posXimg2 = height;
-			}
-			if (posXimg3 < -height) {
-				posXimg3 = height;
-			}
-			
+            if(koins.getY() + 120 < 0) {
+                koins.setPosition(koins.getY(), height);
+            }
+            koins.paint(g);
 	}
 	
-	void drawMatahari(){
+	void drawRoad(){
 		
-		g.setColor(112, 200, 225);
-		g.fillRect(0, 0, getWidth(), getHeight());		
-		g.drawImage(awan, posXAwan, 0, Graphics.LEFT | Graphics.TOP);
-		g.drawImage(awan, posXAwan2, 0, Graphics.LEFT | Graphics.TOP);
-		g.drawImage(awan, posXAwan3, 0, Graphics.LEFT | Graphics.TOP);
-		
-		posXAwan-=1;
-		posXAwan2-=1;
-		posXAwan3-=1;
-		
-		if (posXAwan < -height) {
-			posXAwan = height;
-		}
-		if (posXAwan2 < -height) {
-			posXAwan2 = height;
-		}
-		if (posXAwan3 < -height) {
-			posXAwan3 = height;
-		}
+			roads.setPosition(0, posYRoad1);
+			roads.setTransform(5);
+			roads.paint(g);
+			roads2.setPosition(0, posYRoad2);
+			roads2.setTransform(5);
+			roads2.paint(g);
+			roads3.setPosition(0, posYRoad3);
+			roads3.setTransform(5);
+			roads3.paint(g);
+			
 	}
+
+	void drawMatahari(){
+
+		g.setColor(112, 200, 225);
+		g.fillRect(0, 0, getWidth(), getHeight());
+		g.drawRegion(awan, 0, 0, awan.getWidth(), awan.getHeight(), 5, 0, posYAwan1, Graphics.LEFT | Graphics.BOTTOM);
+		g.drawRegion(awan, 0, 0, awan.getWidth(), awan.getHeight(), 5, 0, posYAwan2, Graphics.LEFT | Graphics.BOTTOM);
+		g.drawRegion(awan, 0, 0, awan.getWidth(), awan.getHeight(), 5, 0, posYAwan3, Graphics.LEFT | Graphics.BOTTOM);			
+
 		
-	void jump(){		
+	}
+
+	void jump(){
 		if (jumping) {
 			tinggi--;
-			cY -= tinggi;
+			cX += tinggi;
 			if (tinggi < 0) {
 				jumping = false;
 				falling = true;
 			}
 		}
-		
+
 		if (falling) {
 			tinggi++;
-			cY += tinggi;
+			cX -= tinggi;
 			if (tinggi > 12) {
 				falling = false;
-				cY = height-runImg.getHeight();
-				koins.setVisible(tampak);
+				cX = 30;
 				onRoad = false;
 			}
-			
+			//biar menapak ke-roadnya
+			else if(spRun.collidesWith(roads2, true)){
+					falling = false;
+	                onRoad = true;
+	                cX = spRun.getX();
+			}
 		}
 		
+		if (onRoad) {
+        	if ((!spRun.collidesWith(roads2, true) && !jumping)) {
+        			falling = true;
+        	}
+        }
+
 	}
-	
+
 }
